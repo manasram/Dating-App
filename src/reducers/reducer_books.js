010f2f680e7178f48d8afb3d5e1c5139ad1d4bfa
manasram <b.manasram@gmail.com>
2017-07-19 21:19:20 -0500

    Initial Commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+    return[
+        {title: 'Javascript'},
+        {title:'Ice and fire'},
+        {title: 'Game of thrones'},
+        {title: 'The hobbit'}
+    ]
+}
